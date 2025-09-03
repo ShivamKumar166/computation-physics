0654757a1e6c08f1fa5a1df23039a799b9a27113
Shivam Kumar <shivam.kumar@niser.ac.in>
2025-09-03 17:03:42 +0530

    Update Assignment 6.py

--- Assignment 6/Assignment 6.py
+++ Assignment 6/Assignment 6.py
@@ -7,9 +7,15 @@ print("Bisection root:", all_lib.bisection(1.5, 3.0))
 print("Regula Falsi root:", all_lib.regula_falsi(1.5, 3.0))
 
 # Question 2
+
 # Test inside [2, 4]
+#Finding the bracket interval
 a, b = 2, 4
 print(all_lib.bracket_interval(a,b))
+
+# the functions are in the all library file
+
+
 
 
 
